381352f791c00fc8a870988168d083658340e0ec
CSHTML5 <contact@cshtml5.com>
2019-02-20 16:03:03 +0100

    Move all the nuspec files into the same folder and automate NuGet package generation via a .bat file + add "NuGet.exe" to the "ExtLibs" folder of the solution.

---
+++ NUGET_PACKAGES/Build NuGet packages.bat
@@ -0,0 +1,7 @@
+..\ExtLibs\NuGet\nuget.exe pack "CSHTML5.Migration.Wrappers.KendoUI.Common.nuspec"
+..\ExtLibs\NuGet\nuget.exe pack "CSHTML5.Migration.Wrappers.KendoUI.Editor.nuspec"
+..\ExtLibs\NuGet\nuget.exe pack "CSHTML5.Migration.Wrappers.KendoUI.Grid.nuspec"
+..\ExtLibs\NuGet\nuget.exe pack "CSHTML5.Wrappers.KendoUI.Common.nuspec"
+..\ExtLibs\NuGet\nuget.exe pack "CSHTML5.Wrappers.KendoUI.Editor.nuspec"
+..\ExtLibs\NuGet\nuget.exe pack "CSHTML5.Wrappers.KendoUI.Grid.nuspec"
+pause
