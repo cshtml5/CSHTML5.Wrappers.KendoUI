a27caeb2e73610019701aea6efdf0ea7bdd8dfd1
Sylvain Maillard <maillard.sy@gmail.com>
2019-03-27 17:42:59 +0100

    Add the generation of the NuGet packages for the TreeView to .bat file.

--- NUGET_PACKAGES/Build NuGet packages.bat
+++ NUGET_PACKAGES/Build NuGet packages.bat
@@ -1,7 +1,9 @@
 ..\ExtLibs\NuGet\nuget.exe pack "CSHTML5.Migration.Wrappers.KendoUI.Common.nuspec"
 ..\ExtLibs\NuGet\nuget.exe pack "CSHTML5.Migration.Wrappers.KendoUI.Editor.nuspec"
 ..\ExtLibs\NuGet\nuget.exe pack "CSHTML5.Migration.Wrappers.KendoUI.Grid.nuspec"
+..\ExtLibs\NuGet\nuget.exe pack "CSHTML5.Migration.Wrappers.KendoUI.TreeView.nuspec"
 ..\ExtLibs\NuGet\nuget.exe pack "CSHTML5.Wrappers.KendoUI.Common.nuspec"
 ..\ExtLibs\NuGet\nuget.exe pack "CSHTML5.Wrappers.KendoUI.Editor.nuspec"
 ..\ExtLibs\NuGet\nuget.exe pack "CSHTML5.Wrappers.KendoUI.Grid.nuspec"
+..\ExtLibs\NuGet\nuget.exe pack "CSHTML5.Wrappers.KendoUI.TreeView.nuspec"
 pause
